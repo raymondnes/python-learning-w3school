0f8acdd3852ea1588e7e96165f8535154aa57ed4
raymondnes <raynelreign@gmali.com>
2023-03-11 23:19:36 +0400

    updated the data type

--- learning-python/practice.py
+++ learning-python/practice.py
@@ -135,3 +135,25 @@ prints
 Python is fantastic
 
 """
+
+# Python Data types
+
+"""
+Example	                                              Data Type
+x = "Hello World"	                                     str	
+x = 20	                                                 int	
+x = 20.5	                                            float	
+x = 1j                                                 complex	
+x = ["apple", "banana", "cherry"]	                    list	
+x = ("apple", "banana", "cherry")                       tuple	
+x = range(6)	                                        range	
+x = {"name" : "John", "age" : 36}           	        dict	
+x = {"apple", "banana", "cherry"}	                    set	
+x = frozenset({"apple", "banana", "cherry"})         frozenset	
+x = True	                                            bool	
+x = b"Hello"	                                       bytes	
+x = bytearray(5)	                                 bytearray	
+x = memoryview(bytes(5))                             memoryview	
+x = None	                                          NoneType	
+
+"""
